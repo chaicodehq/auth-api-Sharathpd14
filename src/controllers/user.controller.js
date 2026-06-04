5fee7f0008c85d363e680665aa15532fd8b51b6c
Sharathpd14 <sharathpd14@gmail.com>
2026-06-04 08:58:08 +0530

    Auth assignment is completed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,6 +9,8 @@ import { User } from '../models/user.model.js';
 export async function listUsers(req, res, next) {
   try {
     // Your code here
+    const users = await User.find()
+    return res.status(200).json({users})
   } catch (error) {
     next(error);
   }
@@ -25,6 +27,13 @@ export async function listUsers(req, res, next) {
 export async function getUser(req, res, next) {
   try {
     // Your code here
+    const id = req.params.id
+    const user = await User.findById(id)
+    if(!user){
+      return res.status(404).json({ error: { message: "User not found" } })
+    }
+
+    return res.status(200).json({user})
   } catch (error) {
     next(error);
   }
@@ -41,6 +50,12 @@ export async function getUser(req, res, next) {
 export async function deleteUser(req, res, next) {
   try {
     // Your code here
+    const user = await User.findByIdAndDelete(req.params.id)
+    if(!user){
+      return res.status(404).json({ error: { message: "User not found" } })
+    } 
+
+    return res.status(200).json({ message: "User deleted successfully" })
   } catch (error) {
     next(error);
   }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -18,9 +18,36 @@ import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema(
   {
     // Your schema fields here
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user"
+    }
   },
   {
     // Schema options here
+    timestamps: true
   }
 );
 
@@ -42,3 +69,15 @@ const userSchema = new mongoose.Schema(
  */
 
 // TODO: Create and export the User model
+
+userSchema.pre("save", async function (next) {
+  if(!this.isModified("password")){
+    next();
+  }
+  this.password = await bcrypt.hash(this.password,10);
+  next();
+})
+
+const User = mongoose.model("User",userSchema);
+
+export  {User}
